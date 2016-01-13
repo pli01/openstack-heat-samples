8ad2b060d614142a51c2e27994cf77c79c12233c
pli01 <filib@laposte.net>
2016-01-13 13:46:48 +0100

    opt parameters

--- bin/launch.sh
+++ bin/launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 test -d $1 || exit 1
 test -z $2 && exit 1
-heat template-validate -f $1/heat.yml -e $1/heat-parameters.yml && \
-heat stack-create $2 -f $1/heat.yml -e $1/heat-parameters.yml
+
+parameters=$1/${3:-heat-parameters.yaml}
+[ -f $parameters ] || exit 1
+heat -k template-validate -f $1/heat.yml -e $parameters && \
+heat -k stack-create $2 -f $1/heat.yml -e $parameters
