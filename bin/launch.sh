a83296b4c7bf9301506d5f9b040ede06df901c85
pli01 <filib@laposte.net>
2016-01-13 15:17:48 +0100

    usage

--- bin/launch.sh
+++ bin/launch.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
-test -d $1 || exit 1
-test -z $2 && exit 1
+
+function usage()
+{
+cat <<EOF
+$(basename $0) stack-dir stack-name [ heat-parameters.yaml ] [ env.yaml ]
+   stack-dir: directory with heat.yaml
+   stack-name: name of the stack to create
+   heat-parameters.yaml: [default: stack-dir/heat-parameters.yaml] yaml file with parameters like flavor,image
+   env.yaml: [default: stack-dir/env.yml] yaml file with resource_registry mapping (ex: Lib::Stack::Web: web.yaml)
+
+ex: 
+  bin/launch.sh instance-boot-volume mystack
+  bin/launch.sh instance-boot-volume mystack heat-parameters-mycloud.yaml
+  bin/launch.sh instance-boot-volume mystack heat-parameters-mycloud.yaml myenv.yaml
+
+EOF
+exit 1
+}
+test -d $1 || usage
+test -z $2 && usage
 
 parameters=$1/${3:-heat-parameters.yaml}
+registry=$1/${4:-env.yaml}
+
+registry_opt="-e $registry"
+[ -f $registry ] || registry_opt=""
+
 [ -f $parameters ] || exit 1
-heat -k template-validate -f $1/heat.yml -e $parameters && \
-heat -k stack-create $2 -f $1/heat.yml -e $parameters
+heat -k template-validate -f $1/heat.yaml $registry_opt -e $parameters && \
+heat -k stack-create $2 -f $1/heat.yaml $registry_opt -e $parameters 
