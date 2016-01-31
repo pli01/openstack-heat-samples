fac99e89809fe499070d05ea87a5676437397e91
pli <philippe.libat@free.fr>
2016-01-31 14:40:13 +0100

    userdata, nopasswd stackato user

--- config-scripts/configure_stackato.sh
+++ config-scripts/configure_stackato.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -x
 
+echo 'stackato ALL = (root) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/nopasswd
+chmod 0440 /etc/sudoers.d/nopasswd
 su - stackato -c '/home/stackato/bin/kato role remove mdns' 
 su - stackato -c '/home/stackato/bin/kato node rename $public_ip.xip.io'
