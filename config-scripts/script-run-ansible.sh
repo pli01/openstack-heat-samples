7dc36e7cf4e650691bf26daa9d997c821aa63e7b
pli <philippe.libat@free.fr>
2016-02-11 00:52:37 +0100

    multi-instance metadata resourcegroup

--- config-scripts/script-run-ansible.sh
+++ config-scripts/script-run-ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export PATH=/usr/local/bin:$PATH
-ansible-playbook -i /root/ansible/hosts /root/ansible/local.yaml -e @/root/ansible/vars.yaml -vv
+ansible-playbook -i 'localhost,' -c local /root/ansible/local.yaml -e @/root/ansible/vars.yaml -vv 2>&1 | tee -a /var/log/run_ansible.log
 
