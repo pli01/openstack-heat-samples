27aa6b28475f71ee7f9f334a7d245f4337a10761
pli <philippe.libat@free.fr>
2016-02-19 23:14:44 +0100

    add vhost jenkins

--- os_image_factory/files/get_jenkins_slave.sh
+++ os_image_factory/files/get_jenkins_slave.sh
@@ -4,4 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -qqy update && apt-get -qqy upgrade
 apt-get -qfy install unzip curl openjdk-7-jre openjdk-7-jdk
 
-curl -O http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/2.0/swarm-client-2.0-jar-with-dependencies.jar
+cd /root
+curl -o /root/swarm-client-2.0-jar-with-dependencies.jar http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/2.0/swarm-client-2.0-jar-with-dependencies.jar
--- os_image_factory/files/run_jenkins_slave.sh
+++ os_image_factory/files/run_jenkins_slave.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-java -jar swarm-client-2.0-jar-with-dependencies.jar -master http://$master_jenkins:8080/ -executors 2 &
+#TODO: run as jenkins user in /var/lib/jenkins
+java -jar /root/swarm-client-2.0-jar-with-dependencies.jar -master http://$master_jenkins:8080/ -executors 2 &
